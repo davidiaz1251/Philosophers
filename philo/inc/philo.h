072ce6906d8f9ff9bb4170fbe7975877a3d3d737
LUIS DAVID <luis.david.10@hotmail.com>
2024-08-31 18:17:01 +0200

    feat: initial commit

---
+++ philo/inc/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldiaz-ra <ldiaz-ra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/31 11:36:45 by ldiaz-ra          #+#    #+#             */
+/*   Updated: 2024/08/31 13:29:58 by ldiaz-ra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>		
+# include <stdlib.h>	
+# include <string.h>	
+# include <unistd.h>	
+# include <sys/time.h>	
+# include <pthread.h>	
+
+
+
+#endif
