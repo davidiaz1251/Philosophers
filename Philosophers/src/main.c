e4092ca308868a0e6414d5dd95e3ac983cf2c309
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-04-18 22:06:10 +0200

    Update file header timestamp in main.c and remove print_philos function

--- Philosophers/src/main.c
+++ Philosophers/src/main.c
@@ -6,27 +6,11 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 16:34:25 by david             #+#    #+#             */
-/*   Updated: 2025/04/18 18:07:59 by david            ###   ########.fr       */
+/*   Updated: 2025/04/18 22:05:52 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-static void	print_philos(t_philo *philos, t_config *cfg)
-{
-	int	i;
-
-	i = 0;
-	while (i < cfg->num_philos)
-	{
-		printf("Filósofo %d\n", philos[i].id);
-		printf("  Left fork : %p\n", (void *)philos[i].left_fork);
-		printf("  Right fork: %p\n", (void *)philos[i].right_fork);
-		printf("  Veces comido: %d\n", philos[i].times_eaten);
-		printf("  Última comida: %ld\n\n", philos[i].last_meal);
-		i++;
-	}
-}
 
 int	main(int argc, char **argv)
 {
@@ -41,7 +25,6 @@ int	main(int argc, char **argv)
 		return (printf("Error: al crear filósofos\n"), 1);
     if (start_simulation(philos, &cfg))
 		return (printf("Error: al iniciar la simulación\n"), 1);
-	// print_philos(philos, &cfg);
 	free_all(philos, &cfg);
 	return (0);
 }
