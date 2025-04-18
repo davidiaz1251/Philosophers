20503a574c7a0a9b7cab62ee1b2d676361d28afe
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-04-18 17:49:31 +0200

    Update file headers with new timestamps and ensure proper formatting

--- Philosophers/src/simulation/routine.c
+++ Philosophers/src/simulation/routine.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 16:37:08 by david             #+#    #+#             */
-/*   Updated: 2025/04/18 16:37:10 by david            ###   ########.fr       */
+/*   Updated: 2025/04/18 17:48:20 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ void	*philo_routine(void *arg)
 
 	philo = (t_philo *)arg;
 	if (philo->id % 2 == 0)
-		usleep(100); // evita deadlocks al inicio
-
+		usleep(100); 
 	while (1)
 	{
 		pthread_mutex_lock(&philo->cfg->death_mutex);
@@ -29,30 +28,19 @@ void	*philo_routine(void *arg)
 			break;
 		}
 		pthread_mutex_unlock(&philo->cfg->death_mutex);
-
-		// Pensar
 		print_status(philo, "is thinking");
-
-		// Coger tenedores
 		pthread_mutex_lock(philo->left_fork);
 		print_status(philo, "has taken a fork");
 		pthread_mutex_lock(philo->right_fork);
 		print_status(philo, "has taken a fork");
-
-		// Comer
 		pthread_mutex_lock(&philo->cfg->death_mutex);
 		philo->last_meal = get_timestamp_ms();
 		pthread_mutex_unlock(&philo->cfg->death_mutex);
-
 		print_status(philo, "is eating");
 		ft_usleep(philo->cfg->time_to_eat);
 		philo->times_eaten++;
-
-		// Soltar tenedores
 		pthread_mutex_unlock(philo->left_fork);
 		pthread_mutex_unlock(philo->right_fork);
-
-		// Dormir
 		print_status(philo, "is sleeping");
 		ft_usleep(philo->cfg->time_to_sleep);
 	}
